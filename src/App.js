b7e1e6a5939d0e197dd072af064eba38650f9ace
Prithivi36 <prithivipappian@gmail.com>
2024-03-18 22:42:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Content from './Content';
+import {Routes ,BrowserRouter,Route} from 'react-router-dom'
+import Test from './Test';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Content />} />
+        <Route path='/hi' element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
