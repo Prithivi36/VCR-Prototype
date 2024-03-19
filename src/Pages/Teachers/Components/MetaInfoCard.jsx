b1227b54ea469479ec52cdbb33e0229fd88fada6
Prithivi36 <prithivipappian@gmail.com>
2024-03-19 07:32:59 +0530

    navigator fix

--- src/Pages/Teachers/Components/MetaInfoCard.jsx
+++ src/Pages/Teachers/Components/MetaInfoCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 function MetaInfoCard() {
+  const navigator=useNavigate()
   return (
     <>
-    <div onClick={()=>window.location.href = '/student/5124852'} className="card flex-row px-3 py-3">
+    <div onClick={()=>navigator('/student/5124852')} className="card flex-row px-3 py-3">
           <table>
             <tr>
               <td className='fw-bolder'>Name</td>
